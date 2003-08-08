a076d3b8f949f14c21064a5588d806a44a2baab4
gc <gc@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2003-08-08 11:33:37 +0000

    fix probe_with (urpmi changed name of option, but my recent checkbutton get_active was also broken)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpmdrake/trunk@17043 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- edit-urpm-sources.pl
+++ edit-urpm-sources.pl
@@ -161,7 +161,7 @@ really want to replace it?"), yesno => 1) or return 0;
 	    $urpm->remove_selected_media;
 	}
 	add_medium_and_check($urpm, N("Please wait, adding medium..."),
-			     { probe_with_hdlist => $info->{hdlist_check}->get_active && $i{hdlist} eq '' },
+			     { probe_with => !$info->{hdlist_check}->get_active || $i{hdlist} eq '' },
 			     $i{name}, $make_url{$type}, $i{hdlist}, update => $type eq 'security');
 	return 1;
     }
