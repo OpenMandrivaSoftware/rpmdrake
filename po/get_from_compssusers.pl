c877c69d77e67486a90d885a4c5e59c49acb6a5f
gc <gc@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2003-09-01 18:56:27 +0000

    be sure to notice when rpmdrake and drakx encodings are different (die)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpmdrake/trunk@17127 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- po/get_from_compssusers.pl
+++ po/get_from_compssusers.pl
@@ -4,7 +4,16 @@ use MDK::Common;
 
 @miss = (qw(Development Server Workstation), 'Graphical Environment');
 
-foreach my $line (cat_("../../../gi/perl-install/share/po/$ARGV[0]")) {
+my $po = $ARGV[0];
+my $drakxfile = "../../../gi/perl-install/share/po/$po";
+
+-e $drakxfile or exit 0;
+
+my ($enc_rpmdrake) = cat_($po) =~ /Content-Type: .*; charset=(.*)\\n/i;
+my ($enc_drakx)    = cat_($drakxfile) =~ /Content-Type: .*; charset=(.*)\\n/;
+uc($enc_rpmdrake) ne uc($enc_drakx) and die "Encodings differ for $po! rpmdrake's encoding: $enc_rpmdrake; drakx's encoding: $enc_drakx";
+
+foreach my $line (cat_($drakxfile)) {
     $line =~ m|^\Q#: ../../share/compssUsers:999| || ($line =~ m|^msgid "([^"]+)"| && grep { $_ eq $1 } @miss) and do {
 	$current = 'inside';
 	print "# DO NOT BOTHER TO MODIFY HERE, BUT IN DRAKX PO\n";
