41ecef1924f10ebe6fd3e7c74178c142a9910f9d
rgarciasuarez <rgarciasuarez@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-06-30 12:30:30 +0000

    Factorize the proxy.cfg code
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpmdrake/trunk@17482 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- grpmi/curl_download/curl_download.pm
+++ grpmi/curl_download/curl_download.pm
@@ -1,34 +1,30 @@
 package curl_download;
 
 use strict;
-use vars qw($VERSION @ISA @EXPORT);
 use MDK::Common;
+use urpm::download;
 
 require DynaLoader;
 
-@ISA = qw(DynaLoader);
-@EXPORT = qw(download readproxy writeproxy);
-$VERSION = '1.0';
+our @ISA = qw(DynaLoader);
+our @EXPORT = qw(download readproxy writeproxy);
+our $VERSION = '1.1';
 
 bootstrap curl_download $VERSION;
 
-sub readproxy {
-    my ($proxy, $proxy_user);
-    foreach (cat_('/etc/urpmi/proxy.cfg')) {
-	/^http_proxy\s*=\s*(.*)$/ and $proxy = $1;
-	/^ftp_proxy\s*=\s*(.*)$/ and $proxy = $1;
-	/^proxy_user\s*=\s*(.*)$/ and $proxy_user = $1;
-    }
-    ($proxy, $proxy_user);
+sub readproxy (;$) {
+    my $proxy = get_proxy(@_);
+    ($proxy->{http_proxy} || $proxy->{ftp_proxy} || '',
+	defined $proxy->{user} ? "$proxy->{user}:$proxy->{pwd}" : '');
 }
 
 sub writeproxy {
     my ($proxy, $proxy_user) = @_;
-    my $f = '/etc/urpmi/proxy.cfg'; 
-    output($f,
-	   if_($proxy, "http_proxy=$proxy\n"),
-	   if_($proxy_user, "proxy_user=$proxy_user\n"));
-    chmod 0600, $f;
+    my ($user, $pwd) = split /:/, $proxy_user;
+    set_proxy_config(user => $user);
+    set_proxy_config(pwd => $pwd);
+    set_proxy_config(http_proxy => $proxy);
+    dump_proxy_config();
 }
 
 sub download {
@@ -38,4 +34,3 @@ sub download {
 }
 
 1;
-
