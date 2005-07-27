3ba396466a0790924e45ea72a7475f37964bec6c
rgarciasuarez <rgarciasuarez@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2005-07-27 09:38:38 +0000

    Update copyright
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpmdrake/trunk@18129 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- rpmdrake.pm
+++ rpmdrake.pm
@@ -1,6 +1,7 @@
 #*****************************************************************************
 # 
-#  Copyright (c) 2002 Guillaume Cottenceau (gc at mandriva dot com)
+#  Copyright (c) 2002 Guillaume Cottenceau
+#  Copyright (c) 2003-2005 Mandriva Linux
 # 
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License version 2, as
