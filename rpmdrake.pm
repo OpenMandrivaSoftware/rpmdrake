132520b59bc9ceb3e9f9d1ffaf6dfd44db9d4d28
tv <tv@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2007-02-28 21:41:13 +0000

    fix setting gettext domains before mygtk2 initilizes
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpmdrake/trunk@22877 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- rpmdrake.pm
+++ rpmdrake.pm
@@ -85,7 +85,9 @@ our @EXPORT = qw(
     strip_first_underscore
 );
 our $typical_width = 280;
-unshift @::textdomains, qw(rpmdrake urpmi rpm-summary-main rpm-summary-contrib rpm-summary-devel);
+
+# i18n: IMPORTANT: to get correct namespace (rpmdrake instead of libDrakX)
+BEGIN { unshift @::textdomains, qw(rpmdrake urpmi rpm-summary-main rpm-summary-contrib rpm-summary-devel) };
 
 use ugtk2 qw(:all);
 ugtk2::add_icon_path('/usr/share/rpmdrake/icons');
