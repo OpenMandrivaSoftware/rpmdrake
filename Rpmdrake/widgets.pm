f99d83487f432afae78d63d8042940cbe88ce716
tv <tv@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2007-03-07 11:34:39 +0000

    split out Gtk2::Mdv::TextView
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpmdrake/trunk@23111 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

---
+++ Rpmdrake/widgets.pm
@@ -0,0 +1,52 @@
+package Gtk2::Mdv::TextView;
+#*****************************************************************************
+#
+#  Copyright (c) 2002 Guillaume Cottenceau
+#  Copyright (c) 2002-2007 Thierry Vignaud <tvignaud@mandriva.com>
+#  Copyright (c) 2003, 2004, 2005 MandrakeSoft SA
+#  Copyright (c) 2005-2007 Mandriva SA
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License version 2, as
+#  published by the Free Software Foundation.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+#*****************************************************************************
+#
+# $Id$
+
+use strict;
+use MDK::Common::Func 'any';
+use lib qw(/usr/lib/libDrakX);
+
+use mygtk2 qw(gtknew);  #- do not import anything else, especially gtkadd() which conflicts with ugtk2 one
+use ugtk2 qw(:all);
+use Time::HiRes;
+
+
+sub new {
+    my ($_class) = @_;
+    my $w = mygtk2::gtknew('TextView');
+    my $time if 0;
+    $w->signal_connect(size_allocate => sub {
+        my ($w, $requisition) = @_;
+        return if !ref($w->{anchors});
+        return if Time::HiRes::clock_gettime() - $time < 0.200;
+        $time = Time::HiRes::clock_gettime();
+        foreach my $anchor (@{$w->{anchors}}) {
+            $_->set_size_request($requisition->width-30, -1) foreach $anchor->get_widgets;
+        }
+        1;
+    });
+    $w;
+}
+
+1;
