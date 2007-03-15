b252552bb17ea6e6180224015aeb80b40043426f
tv <tv@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2007-03-15 20:12:04 +0000

    (get_pkgs) revert a bogus change that went in r144310
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpmdrake/trunk@23310 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- Rpmdrake/pkg.pm
+++ Rpmdrake/pkg.pm
@@ -214,7 +214,7 @@ sub get_pkgs {
 	    if (@update_medias > 0) {
 		if (!$opts->{skip_updating_mu} && !$is_update_media_already_asked) {
               $is_update_media_already_asked = 1;
-		    $ask_confirmation and interactive_msg_with_banner(N("Confirmation"),
+		     $::options{'no-confirmation'} or interactive_msg_with_banner(N("Confirmation"),
 N("I need to contact the mirror to get latest update packages.
 Please check that your network is currently running.
 
