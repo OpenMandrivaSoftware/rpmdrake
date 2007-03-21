4e2066e92fe9826a612a1477ac0db20bf8ba6682
tv <tv@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2007-03-21 15:00:14 +0000

    (perform_installation) ask confirmation _before_ actually removing packages
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpmdrake/trunk@23413 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- Rpmdrake/pkg.pm
+++ Rpmdrake/pkg.pm
@@ -496,9 +496,6 @@ sub perform_installation {  #- (partially) duplicated from /usr/sbin/urpmi :-(
     @{$urpm->{ask_remove}} = sort urpm::select::removed_packages($urpm, $urpm->{state});
     my @to_remove = grep { $_ } map { if_($pkgs->{$_}{selected}, $pkgs->{$_}{urpm_name}) } keys %$pkgs;
 
-    # select packages to uninstall for !update mode:
-    perform_removal($urpm, { map { my $p = $pkgs->{$_}; $p->{selected} && $p->{pkg}->flag_installed ? ($_ => $p) : () } keys %$pkgs }) if !$probe_only_for_updates;
-
     my $r = join "\n", urpm::select::translate_why_removed($urpm, $urpm->{state}, @to_remove);
 
     my $install_count = int(@pkgs);
@@ -519,6 +516,9 @@ sub perform_installation {  #- (partially) duplicated from /usr/sbin/urpmi :-(
                      };
 
     my $_guard = before_leaving { urpm::removable::try_umounting_removables($urpm) };
+
+    # select packages to uninstall for !update mode:
+    perform_removal($urpm, { map { my $p = $pkgs->{$_}; $p->{selected} && $p->{pkg}->flag_installed ? ($_ => $p) : () } keys %$pkgs }) if !$probe_only_for_updates;
 
     Rpmdrake::gurpm::init(1 ? N("Please wait") : N("Package installation..."), N("Initializing..."), transient => $::main_window);
     my $canceled;
