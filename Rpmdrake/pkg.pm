7d48496bf467591048553409626c33faab216e56
tv <tv@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2007-09-11 13:31:32 +0000

    (open_urpmi_db) shrink opening urpmi DB time by 30% (#33334)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpmdrake/trunk@227617 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- Rpmdrake/pkg.pm
+++ Rpmdrake/pkg.pm
@@ -251,7 +251,7 @@ sub open_urpmi_db() {
                          N("A fatal error occurred: %s.", $_[1]));
     };
     my $media = ref $::rpmdrake_options{media} ? join(',', @{$::rpmdrake_options{media}}) : '';
-    urpm::media::configure($urpm, media => $media);
+    urpm::media::read_config($urpm);
 
     my $searchmedia = join(',', map { $_->{name} } grep { $_->{ignore} && $_->{name} =~ /backport/i } @{$urpm->{media}});
     urpm::media::configure($urpm, media => $media, if_($searchmedia, searchmedia => $searchmedia));
