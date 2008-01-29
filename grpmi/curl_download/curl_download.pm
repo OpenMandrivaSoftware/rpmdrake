615f1e1f632cd96f676bc7124dde71e1476c12d8
tv <tv@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2008-01-29 23:01:15 +0000

    drop now useless curl XS binding
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpmdrake/trunk@233588 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- grpmi/curl_download/curl_download.pm
+++ grpmi/curl_download/curl_download.pm
@@ -3,12 +3,7 @@ package curl_download;
 use strict;
 use urpm::download;
 
-require DynaLoader;
-
-our @ISA = qw(DynaLoader);
 our $VERSION = '1.2';
-
-bootstrap curl_download $VERSION;
 
 sub readproxy (;$) {
     my $proxy = get_proxy($_[0]);
@@ -23,12 +18,6 @@ sub writeproxy {
     set_proxy_config(pwd => $pwd, $o_media_name);
     set_proxy_config(http_proxy => $proxy, $o_media_name);
     dump_proxy_config();
-}
-
-sub download {
-    my ($url, $location, $downloadprogress_callback) = @_;
-
-    download_real($url, $location, $downloadprogress_callback, readproxy());
 }
 
 1;
