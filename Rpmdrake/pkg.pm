OpenMandrivaSoftware/rpmdrake

commit d46838c1454f283958da959488efee2bb4c943ce

(extract_header) do not display the download progress dialog when not downloading, only a statusbar message
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpmdrake/trunk@233980 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

Rpmdrake/pkg.pm
@@ -107,13 +107,16 @@ sub extract_header {
                          changelog => $chg_prepro->(to_utf8(scalar(run_rpm("rpm -q --changelog $name")))) });
     } else {
 	my $medium = pkg2medium($p, $urpm);
-        my ($local_source, %xml_info_pkgs);
+        my ($local_source, %xml_info_pkgs, $bar_id);
+        my $_statusbar_clean_guard = before_leaving { $bar_id and statusbar_msg_remove($bar_id) };
         if (my $dir = urpm::file_from_local_url($medium->{url})) {
             $local_source = "$dir/" . $p->filename;
             $urpm->{log}("getting information from rpms from $dir");
         } else {
-                my $gurpm = Rpmdrake::gurpm->new(N("Please wait"), transient => $::main_window);
+            my $gurpm;
+            $bar_id = statusbar_msg(N("Getting '%s' from XML meta-data...", $xml_info), 0);
                 if (my $xml_info_file = urpm::media::any_xml_info($urpm, $medium, $xml_info, undef, sub {
+                                                                      $urpm ||= Rpmdrake::gurpm->new(N("Please wait"), transient => $::main_window);
                                                                       download_callback($gurpm, @_)
                                                                         or goto header_non_available;
                                                                   })) {

commit 97ad74a4a9de3be04e9e59e5924f0fce87a5c1bd

(extract_header) fix a regression (not downloading XML metada) introduced in r233980 on 2008-02-06 (was "do not display the download progress dialog when not downloading, only a statusbar message")
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpmdrake/trunk@234005 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

Rpmdrake/pkg.pm
@@ -130,7 +130,7 @@ sub extract_header {
             $bar_id = statusbar_msg(N("Getting '%s' from XML meta-data...", $xml_info), 0);
             my $_gurpm_clean_guard = before_leaving { undef $gurpm };
                 if (my $xml_info_file = urpm::media::any_xml_info($urpm, $medium, $xml_info, undef, sub {
-                                                                      $urpm ||= Rpmdrake::gurpm->new(N("Please wait"), transient => $::main_window);
+                                                                      $gurpm ||= Rpmdrake::gurpm->new(N("Please wait"), transient => $::main_window);
                                                                       download_callback($gurpm, @_)
                                                                         or goto header_non_available;
                                                                   })) {