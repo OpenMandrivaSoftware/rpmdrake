86dc44569d1d3eccc8089099d9e3a32f9cda20f7
tv <tv@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2008-02-06 14:11:08 +0000

    (extract_header) make sure we destroy the download progress dialog on error
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpmdrake/trunk@233981 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- Rpmdrake/pkg.pm
+++ Rpmdrake/pkg.pm
@@ -115,6 +115,7 @@ sub extract_header {
         } else {
             my $gurpm;
             $bar_id = statusbar_msg(N("Getting '%s' from XML meta-data...", $xml_info), 0);
+            my $_gurpm_clean_guard = before_leaving { undef $gurpm };
                 if (my $xml_info_file = urpm::media::any_xml_info($urpm, $medium, $xml_info, undef, sub {
                                                                       $urpm ||= Rpmdrake::gurpm->new(N("Please wait"), transient => $::main_window);
                                                                       download_callback($gurpm, @_)
