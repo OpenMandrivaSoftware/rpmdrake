OpenMandrivaSoftware/rpmdrake

commit 6f2ff801e94b391f948aedc3226080fe7169b43c

(extract_header) fix file list of installed RPMS by forcing list context (and simplfiy the code btw)
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpmdrake/trunk@233988 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

Rpmdrake/pkg.pm
@@ -111,7 +111,8 @@ sub extract_header {
     $name =~ s!\.src!!;
 
     if ($p->flag_installed && !$p->flag_upgrade) {
-	add2hash($pkg, { files => [ split /\n/, chomp_(to_utf8(scalar(run_rpm("rpm -ql $name")))) || N("(none)") ],
+        my @files = map { chomp_(to_utf8($_)) } run_rpm("rpm -ql rpm");
+	add2hash($pkg, { files => [ @files ? @files : N("(none)") ],
                          changelog => $chg_prepro->(to_utf8(scalar(run_rpm("rpm -q --changelog $name")))) });
     } else {
 	my $medium = pkg2medium($p, $urpm);

commit efe5756da3fe73dd4bb62ae9f46c1d06a953fefa

(extract_header) fix querying file list of installed packages (regression introduced in r233988 on 2008-02-06 ("fix file list of installed RPMS...")
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpmdrake/trunk@234086 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

Rpmdrake/pkg.pm
@@ -106,7 +106,7 @@ sub extract_header {
     $name =~ s!\.src!!;
 
     if ($p->flag_installed && !$p->flag_upgrade) {
-        my @files = map { chomp_(to_utf8($_)) } run_rpm("rpm -ql rpm");
+        my @files = map { chomp_(to_utf8($_)) } run_rpm("rpm -ql $name");
 	add2hash($pkg, { files => [ @files ? @files : N("(none)") ],
                          changelog => format_changelog_string(to_utf8(scalar(run_rpm("rpm -q --changelog $name")))) });
     } else {