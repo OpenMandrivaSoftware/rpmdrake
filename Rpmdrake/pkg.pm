e9a2d946486e51f2d49db0e02432fa4fcaa355f8
tv <tv@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2008-02-13 14:12:32 +0000

    (extract_header,run_rpm) factorize calling ensure_utf8() in run_rpm(), thus enabling to fix UTF-8-ness per-line, thus reducing the number of garbaged accents in changelogs
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpmdrake/trunk@234630 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- Rpmdrake/pkg.pm
+++ Rpmdrake/pkg.pm
@@ -94,7 +94,7 @@ sub run_rpm {
     foreach (qw(LANG LC_CTYPE LC_NUMERIC LC_TIME LC_COLLATE LC_MONETARY LC_MESSAGES LC_PAPER LC_NAME LC_ADDRESS LC_TELEPHONE LC_MEASUREMENT LC_IDENTIFICATION LC_ALL)) {
         local $ENV{$_} = $ENV{$_} . '.UTF-8' if $ENV{$_} && $ENV{$_} !~ /UTF-8/;
     }
-    my @l = `@_`;
+    my @l = map { ensure_utf8($_) } `@_`;
     wantarray() ? @l : join('', @l);
 }
 
@@ -115,9 +115,9 @@ sub extract_header {
     $name =~ s!\.src!!;
 
     if ($p->flag_installed && !$p->flag_upgrade) {
-        my @files = map { chomp_(ensure_utf8($_)) } run_rpm("rpm -ql $name");
+        my @files = map { chomp_($_) } run_rpm("rpm -ql $name");
 	add2hash($pkg, { files => [ @files ? @files : N("(none)") ],
-                         changelog => format_changelog_string($o_installed_version, ensure_utf8(scalar(run_rpm("rpm -q --changelog $name")))) });
+                         changelog => format_changelog_string($o_installed_version, scalar(run_rpm("rpm -q --changelog $name"))) });
     } else {
 	my $medium = pkg2medium($p, $urpm);
         my ($local_source, %xml_info_pkgs, $bar_id);
