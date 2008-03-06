0aa6407a6c21143d2962d36dc00b0ec8ce988ef4
tv <tv@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2008-03-06 16:48:18 +0000

    (resolve_dependencies) don't select all updates by default in rpmdrake (#38611)
    regression introduced in r238520 on 2008-03-04 in order to handle
    priority upgrade list in 4.3.2: "(get_pkgs) use more modern
    urpm::select::resolve_dependencies() API instead of partially
    open-coding it, thus enabling further improvements"
    
    
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpmdrake/trunk@238735 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- Rpmdrake/pkg.pm
+++ Rpmdrake/pkg.pm
@@ -421,7 +421,8 @@ sub get_pkgs {
         $urpm, $state, $requested,
         callback_choices => \&Rpmdrake::gui::callback_choices,
         priority_upgrade => $urpm->{options}{'priority-upgrade'},
-        auto_select => 1,
+        auto_select => $probe_only_for_updates,
+        only_request_packages_to_upgrade => 1,
         upgrade_callback => sub {
             my ($requested) = @_;
             @requested = sort map { urpm_name($_) } @{$urpm->{depslist}}[keys %$requested];
