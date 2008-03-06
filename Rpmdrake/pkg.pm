OpenMandrivaSoftware/rpmdrake

commit ad55d2b6b6d224a34659620fc2abf81bdeb5aa05

(get_pkgs) use more modern urpm::select::resolve_dependencies() API instead of partially open-coding it, thus enabling further improvements
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpmdrake/trunk@238520 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

Rpmdrake/pkg.pm
@@ -403,22 +403,25 @@ sub get_pkgs {
 
     my $requested = {};
     my $state = {};
-    $urpm->request_packages_to_upgrade(
-	$db,
-	$state,
-	$requested,
-    );
 
     # list of updates (including those matching /etc/urpmi/skip.list):
-    my @requested = sort map { urpm_name($_) } @{$urpm->{depslist}}[keys %$requested];
+    my @requested;
+
     # list of pure updates (w/o those matching /etc/urpmi/skip.list but with their deps):
-    my @requested_strict = $probe_only_for_updates ?
-      sort map {
-          urpm_name($_);
-      } $urpm->resolve_requested($db, $state, $requested, callback_choices => \&Rpmdrake::gui::callback_choices)
-        : ();
-    # list updates including skiped ones + their deps in MandrivaUpdate:
-    push @requested, difference2(\@requested_strict, \@requested) if $probe_only_for_updates;
+    my @requested_strict;
+
+    urpm::select::resolve_dependencies(
+        $urpm, $state, $requested,
+        callback_choices => \&Rpmdrake::gui::callback_choices,
+        priority_upgrade => $urpm->{options}{'priority-upgrade'},
+        auto_select => 1,
+        upgrade_callback => sub {
+            @requested = sort map { urpm_name($_) } @{$urpm->{depslist}}[keys %$requested];
+        },
+        if_($probe_only_for_updates,
+           resolve_req_callback => sub { @requested_strict = sort map { urpm_name($_) } @_ }
+       ),
+    );
 
     if (!$probe_only_for_updates) {
         $urpm->compute_installed_flags($db); # TODO/FIXME: not for updates

commit aaadd22a73266fe7c900fd1e835f83ea177d8e45

(get_pkgs) fix not listing all updates (#38595) ; regression introduced in r238520 on 2008-03-04 while adding priority upgrade list support for 4.3.2:
"(get_pkgs) use more modern urpm::select::resolve_dependencies() API
instead of partially open-coding it, thus enabling further
improvements"


git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpmdrake/trunk@238731 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

Rpmdrake/pkg.pm
@@ -423,12 +423,17 @@ sub get_pkgs {
         priority_upgrade => $urpm->{options}{'priority-upgrade'},
         auto_select => 1,
         upgrade_callback => sub {
+            my ($requested) = @_;
             @requested = sort map { urpm_name($_) } @{$urpm->{depslist}}[keys %$requested];
         },
         if_($probe_only_for_updates,
            resolve_req_callback => sub { @requested_strict = sort map { urpm_name($_) } @_ }
        ),
     );
+
+    # list updates including skiped ones + their deps in MandrivaUpdate:
+    push @requested, difference2(\@requested_strict, \@requested) if $probe_only_for_updates;
+
     $priority_state = $restart_itself ? $state : undef;
     $priority_requested = $restart_itself ? $requested : undef;
 