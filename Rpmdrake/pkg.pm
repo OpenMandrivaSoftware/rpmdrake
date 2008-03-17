29159a7af0247d645c3c7d958857dd52e2c44cbd
tv <tv@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2008-03-17 15:31:17 +0000

    add tooltips to pull down menus
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpmdrake/trunk@239826 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- Rpmdrake/pkg.pm
+++ Rpmdrake/pkg.pm
@@ -809,7 +809,7 @@ you may now inspect some in order to take actions:"),
 
     N("RPM transaction %d/%d");
     N("Unselect all");
-    N("Details"); N("Filter");
+    N("Details");
 
     statusbar_msg_remove($statusbar_msg_id); #- XXX maybe remove this
 
