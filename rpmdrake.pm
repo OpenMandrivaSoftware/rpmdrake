OpenMandrivaSoftware/rpmdrake

commit 6c0ff61cd468d331f703114866fc8030b23b3c04

(interactive_msg) enable to use markups in TextView (needed for next commit)
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpmdrake/trunk@238921 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

rpmdrake.pm
@@ -195,7 +195,11 @@ sub interactive_msg {
     local $::isEmbedded;
     my $d = ugtk2->new($title, grab => 1, if_(exists $options{transient}, transient => $options{transient}));
     $d->{rwindow}->set_position($options{transient} ? 'center_on_parent' : 'center_always');
-    $contents = formatAlaTeX($contents) unless $options{scroll}; #- because we'll use a WrappedLabel
+    if ($options{scroll}) {
+        $contents = ugtk2::markup_to_TextView_format($contents);
+    } else { #- because we'll use a WrappedLabel
+        $contents = formatAlaTeX($contents);
+    }
     my $banner = $options{banner} ? getbanner() : undef;
     my $text_w;
     gtkadd(

commit ca44bd83dea0c6d7c2a0d377bc08557aacbbf3f8

(interactive_msg) fix displaying data about packages when choosing one while resolving dependancies (#39042)
(regression introduced in r238921 on 2008-03-10: "(interactive_msg)
enable to use markups in TextView (needed for next commit)")


git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpmdrake/trunk@240025 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

rpmdrake.pm
@@ -201,9 +201,9 @@ sub interactive_msg {
     my $d = ugtk2->new($title, grab => 1, if_(exists $options{transient}, transient => $options{transient}));
     $d->{rwindow}->set_position($options{transient} ? 'center_on_parent' : 'center_always');
     if ($options{scroll}) {
-        $contents = ugtk2::markup_to_TextView_format($contents);
+        $contents = ugtk2::markup_to_TextView_format($contents) if !ref $contents;
     } else { #- because we'll use a WrappedLabel
-        $contents = formatAlaTeX($contents);
+        $contents = formatAlaTeX($contents) if !ref $contents;
     }
     my $banner = $options{banner} ? getbanner() : undef;
     my $text_w;