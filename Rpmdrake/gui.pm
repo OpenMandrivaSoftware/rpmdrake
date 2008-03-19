OpenMandrivaSoftware/rpmdrake

commit b004c952b9135442b126d179fcc3e3aaeb4840d0

(build_tree,run_treeview_dialog) do the sort when filling in the tree in "by_group" mode rather than setting it as a tree propriety (needed before resurecting flat mode)
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpmdrake/trunk@239885 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

Rpmdrake/gui.pm
@@ -865,7 +865,7 @@ or you already installed all of them."));
             #- we populate all the groups tree at first
             %$elems = ();
             # better loop on packages, create groups tree and push packages in the proper place:
-            foreach my $pkg (@elems) {
+            foreach my $pkg (sort @elems) {
                 my $grp = $pkg->[1];
                 add_parent($grp);
                 $elems->{$grp} ||= [];

rpmdrake
@@ -290,7 +290,7 @@ sub run_treeview_dialog {
     $columns{name}{renderer}->set_property('ellipsize', 'end');
     $columns{$_}{renderer}->set_property('xpad', '6') foreach @columns;
     $columns{name}{widget}->set_resizable(1); 
-    $detail_list_model->set_sort_column_id($pkg_columns{text}, 'ascending');
+    #$detail_list_model->set_sort_column_id($pkg_columns{text}, 'ascending');
     $detail_list_model->set_sort_func($pkg_columns{version}, \&sort_callback);
     $detail_list->set_rules_hint(1);
 

commit 96ae7b4cf8f5487913f6f27e90ee19d914290533

(build_tree) restore sorting packages in 'by_group' view ; regression introduced in r239885 on 2008-03-17:
"(build_tree,run_treeview_dialog) do the sort when filling in the tree
in "by_group" mode rather than setting it as a tree propriety (needed
before resurecting flat mode)"


git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpmdrake/trunk@240075 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

Rpmdrake/gui.pm
@@ -833,7 +833,7 @@ or you already installed all of them."));
                 $explanation_only_once = 1;
             }
         }
-        @elems = map { [ $_, !$flat && ctreefy($pkgs->{$_}{pkg}->group) ] } @keys;
+        @elems = map { [ $_, !$flat && ctreefy($pkgs->{$_}{pkg}->group) ] } sort @keys;
     }
     my %sortmethods = (
         by_size => sub { sort { $pkgs->{$b->[0]}{pkg}->size <=> $pkgs->{$a->[0]}{pkg}->size } @_ },
@@ -865,7 +865,7 @@ or you already installed all of them."));
             #- we populate all the groups tree at first
             %$elems = ();
             # better loop on packages, create groups tree and push packages in the proper place:
-            foreach my $pkg (sort @elems) {
+            foreach my $pkg (@elems) {
                 my $grp = $pkg->[1];
                 add_parent($grp);
                 $elems->{$grp} ||= [];