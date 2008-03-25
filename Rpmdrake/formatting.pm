a28600076f4360ada2d27af77af1324fb6631871
tv <tv@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2008-03-25 13:29:14 +0000

    (pkg2medium) just skip non existing packages (#38793)
    [I suspect something like the bug fixed by commit r234617 on
    2008-02-13:
    
    "(node_state) just skip non existing packages (#36529)
    
    (eg: when rpmdrake download info.xml.lzma on demand when searching or
    browsing whereas the package was updated in the mean time, the info
    file references the newer package whereas urpmi database only know the
    older version)"]
    
    
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpmdrake/trunk@240614 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- Rpmdrake/formatting.pm
+++ Rpmdrake/formatting.pm
@@ -98,6 +98,7 @@ sub urpm_name {
 
 sub pkg2medium {
     my ($p, $urpm) = @_;
+    return if !ref $p;
     return { name => N("None (installed)") } if !$p->id; # if installed
     URPM::pkg2media($urpm->{media}, $p) || undef;
 }
