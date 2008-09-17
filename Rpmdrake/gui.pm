OpenMandrivaSoftware/rpmdrake

commit 0f438c51cc4110f1d162d4d08f52f1e0cf88c07e

(format_pkg_simplifiedinfo) urpmi-4.10.10 enables to list all packages update description w/o having the ia32 one overwriting/hiding the x86_64 (or the reverse)
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpmdrake/trunk@227983 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

Rpmdrake/gui.pm
@@ -82,7 +82,7 @@ sub format_pkg_simplifiedinfo {
     my ($name, $version) = split_fullname($key);
     my $raw_medium = pkg2medium($pkgs->{$key}{pkg}, $urpm);
     my $medium = $raw_medium ? $raw_medium->{name} : undef;
-    my $update_descr = $descriptions->{$name};
+    my $update_descr = $descriptions->{$medium}{$name};
     # discard update fields if not matching:
     my $is_update = ($pkgs->{$key}{pkg}->flag_upgrade && $update_descr && $update_descr->{pre});
     my $summary = get_summary($key);

commit c87821680086be22ebaa3506c83f3aadd19ca21a

(pkgs_provider) fix listing updates per importance (#41331) ; regression introduced in r227983 on 2007-09-14:
"(format_pkg_simplifiedinfo) urpmi-4.10.10 enables to list all packages
update description w/o having the ia32 one overwriting/hiding the
x86_64 (or the reverse)"


git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpmdrake/trunk@245301 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

Rpmdrake/gui.pm
@@ -542,9 +542,11 @@ sub pkgs_provider {
     );
     foreach my $importance (qw(bugfix security normal)) {
         $tmp_filter_methods{$importance} = sub {
+            my @media = keys %$descriptions;
             [ grep { 
                 my ($name) = split_fullname($_);
-                $descriptions->{$name}{importance} eq $importance } @{$h->{updates}} ];
+                my $medium = find { $descriptions->{$_}{$name} } @media;
+                $medium && $descriptions->{$medium}{$name}{importance} eq $importance } @{$h->{updates}} ];
         };
     }
 