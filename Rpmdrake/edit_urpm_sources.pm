1129142809425b8879d0a781dabd55dd7f2c9511
tv <tv@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2009-01-08 11:43:19 +0000

    use english case (spoted by pixel)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpmdrake/trunk@250977 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- Rpmdrake/edit_urpm_sources.pm
+++ Rpmdrake/edit_urpm_sources.pm
@@ -414,7 +414,7 @@ sub remove_callback() {
 	N("Source Removal"),
 	@rows == 1 ?
 	  N("Are you sure you want to remove source \"%s\"?", to_utf8($urpm->{media}[$rows[0]]{name})) :
-	    N("Are you sure you want to remove the following sources ?") . "\n\n" .
+	    N("Are you sure you want to remove the following sources?") . "\n\n" .
 	      format_list(map { to_utf8($urpm->{media}[$_]{name}) } @rows),
 	yesno => 1, scroll => 1,
 	 transient => $::main_window,
