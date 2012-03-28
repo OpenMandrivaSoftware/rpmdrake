83ca0fcce4443df139edef866b67d3e252d4d455
tv@5b7c3c08-40e5-403b-9995-ace06908e4af <tv@5b7c3c08-40e5-403b-9995-ace06908e4af>
2012-03-28 20:43:53 +0000

    (get_pkgs) consider name+arch as key when filtering out older packages/updates, thus fixing only listing first arch seen (mga#5047)
    regression introduced in r3601 on March 20 2012:
        (get_pkgs) only display latest updates, not all of them (mga#2258, mga#4534)
    
        git-svn-id: svn+ssh://svn.mageia.org/svn/soft/rpmdrake/trunk@3601 5b7c3c08-40e5-403b-9995-ace06908e4af
    
    git-svn-id: svn://svn.mageia.org/soft/rpmdrake/trunk@3876 5b7c3c08-40e5-403b-9995-ace06908e4af

--- Rpmdrake/pkg.pm
+++ Rpmdrake/pkg.pm
@@ -524,7 +524,7 @@ sub get_pkgs {
     foreach my $pkg (@{$urpm->{depslist}}) {
         update_pbar($gurpm);
 	$pkg->flag_upgrade or next;
-	my $short_name = $pkg->name;
+	my $short_name = $pkg->name . $pkg->arch;
 	$l{$short_name} = $pkg if !$l{$short_name} || $l{$short_name}->compare($pkg);
     }
     my @installable_pkgs = map { my $n = $_->fullname; $all_pkgs{$n} = { pkg => $_ }; $n } values %l;

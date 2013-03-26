OpenMandrivaSoftware/rpmdrake

commit 1d4a2bec355e5175cd4d3461bb9cae6fb1ad0d4f

(get_pkgs) only display latest updates, not all of them (mga#2258, mga#4534)
git-svn-id: svn://svn.mageia.org/soft/rpmdrake/trunk@3601 5b7c3c08-40e5-403b-9995-ace06908e4af

Rpmdrake/pkg.pm
@@ -516,15 +516,16 @@ sub get_pkgs {
     $urpm->{rpmdrake_state} = $state; #- Don't forget it
     $gurpm->progress($level = 0.7);
 
-    my @installable_pkgs;
+    my %l;
     reset_pbar_count(1);
     foreach my $pkg (@{$urpm->{depslist}}) {
         update_pbar($gurpm);
 	$pkg->flag_upgrade or next;
-        my $name = $pkg->fullname;
-        push @installable_pkgs, $name;
-        $all_pkgs{$name} = { pkg => $pkg };
+	my $short_name = $pkg->name;
+	$l{$short_name} = $pkg if !$l{$short_name} || $l{$short_name}->compare($pkg);
     }
+    my @installable_pkgs = map { my $n = $_->fullname; $all_pkgs{$n} = { pkg => $_ }; $n } values %l;
+    undef %l;
 
     my @inactive_backports;
     my @active_backports;

commit 83ca0fcce4443df139edef866b67d3e252d4d455

(get_pkgs) consider name+arch as key when filtering out older packages/updates, thus fixing only listing first arch seen (mga#5047)
regression introduced in r3601 on March 20 2012:
    (get_pkgs) only display latest updates, not all of them (mga#2258, mga#4534)

    git-svn-id: svn+ssh://svn.mageia.org/svn/soft/rpmdrake/trunk@3601 5b7c3c08-40e5-403b-9995-ace06908e4af

git-svn-id: svn://svn.mageia.org/soft/rpmdrake/trunk@3876 5b7c3c08-40e5-403b-9995-ace06908e4af

Rpmdrake/pkg.pm
@@ -524,7 +524,7 @@ sub get_pkgs {
     foreach my $pkg (@{$urpm->{depslist}}) {
         update_pbar($gurpm);
 	$pkg->flag_upgrade or next;
-	my $short_name = $pkg->name;
+	my $short_name = $pkg->name . $pkg->arch;
 	$l{$short_name} = $pkg if !$l{$short_name} || $l{$short_name}->compare($pkg);
     }
     my @installable_pkgs = map { my $n = $_->fullname; $all_pkgs{$n} = { pkg => $_ }; $n } values %l;