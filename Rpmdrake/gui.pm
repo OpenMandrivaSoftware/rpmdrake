OpenMandrivaSoftware/rpmdrake

commit ab09e04c4674ada56575157b4b47b76c511ce234

(get_info) provide version to extract_header() thus fixing changelog highlighting
git-svn-id: svn://svn.mageia.org/soft/rpmdrake/trunk@5401 5b7c3c08-40e5-403b-9995-ace06908e4af

Rpmdrake/gui.pm
@@ -1072,7 +1072,8 @@ sub get_info {
     #- the package information hasn't been loaded. Instead of rescanning the media, just give up.
     exists $pkgs->{$key} or return [ [ N("Description not available for this package\n") ] ];
     #- get the description if needed:
-    exists $pkgs->{$key}{description} or slow_func($widget, sub { extract_header($pkgs->{$key}, $urpm, 'info') });
+    
+    exists $pkgs->{$key}{description} or slow_func($widget, sub { extract_header($pkgs->{$key}, $urpm, 'info', version_release($pkgs->{$key}{pkg})) });
     format_pkg_simplifiedinfo($pkgs, $key, $urpm, $descriptions);
 }
 

commit 3e0133e8106d05e5b46c7bb30624872959f6ebbc

(get_info) fix provided installed version for comparing
we were providing the version of the package we were looking at...

"bug" introduced in commit r5401 on 2012-08-24:
"(get_info) provide version to extract_header() thus fixing changelog
highlighting"

git-svn-id: svn://svn.mageia.org/soft/rpmdrake/trunk@5986 5b7c3c08-40e5-403b-9995-ace06908e4af

Rpmdrake/gui.pm
@@ -1076,7 +1076,7 @@ sub get_info {
     exists $pkgs->{$key} or return [ [ N("Description not available for this package\n") ] ];
     #- get the description if needed:
     
-    exists $pkgs->{$key}{description} or slow_func($widget, sub { extract_header($pkgs->{$key}, $urpm, 'info', version_release($pkgs->{$key}{pkg})) });
+    exists $pkgs->{$key}{description} or slow_func($widget, sub { extract_header($pkgs->{$key}, $urpm, 'info', find_installed_version($pkgs->{$key}{pkg})) });
     format_pkg_simplifiedinfo($pkgs, $key, $urpm, $descriptions);
 }
 